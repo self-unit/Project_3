cdc15989b10d58cb11798d179870fa86530e5f81
self-unit <self_unit@hotmail.com>
2018-11-05 13:21:12 +0000

    first commit

---
+++ backend/src/main/java/com/codeclan/project/Project3/controllers/SearchController.java
@@ -0,0 +1,10 @@
+package com.codeclan.project.Project3.controllers;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/request/{search}")
+public class SearchController {
+}
