a637960a926bcd5baeabcd4d991921d7e3e5af77
kaluzsikc <kaluzsikc@gmail.com>
2018-11-05 14:19:59 +0000

    Set state in MainBox. Search form is taking input text and currrency. Form gets submitted

--- frontend/src/containers/MainBox.js
+++ frontend/src/containers/MainBox.js
@@ -6,12 +6,51 @@ import LoadingBox from './LoadingBox';
 
 class Main extends Component {
 
+    constructor(props) {
+        super(props)
+        this.state = {
+            text: '',
+            currency: '',
+            results: []
+        }
+        this.handleInput = this.handleInput.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    getInfo = () => {
+        //fetch request
+    }
+
+    handleInput(event) {
+        this.setState({ text: event.target.value })
+    }
+
+    handleChange(event) {
+        this.setState({ currency: event.target.value })
+    }
+
+    handleSubmit(event) {
+        event.preventDefault();
+        console.log('form submitted');
+        const text = this.state.text;
+        const currency = this.state.currency;
+
+        console.log('text:', text);
+        console.log('currency:', currency);
+
+        //reset form
+        this.setState({ text: '', currency: '' })
+    }
+
     render() {
         return (
             <Router>
                 <React.Fragment>
-                    <Route exact path="/" component={SearchBox} />
-                    <Route path="/results" component={Results} />
+                    <Route exact path="/"
+                        render={() => <SearchBox handleInput={this.handleInput} handleChange={this.handleChange} handleSubmit={this.handleSubmit} search={this.state.text} currency={this.state.currency} />}
+                    />
+                    <Route path="/results" render={() => <Results results={this.state.results} />} />
                     <Route path="/loading" component={LoadingBox} />
                 </React.Fragment>
             </Router>
--- frontend/src/containers/SearchBox.js
+++ frontend/src/containers/SearchBox.js
@@ -1,94 +1,36 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 
-class SearchBox extends Component {
+const SearchBox = ({search, currency, handleSubmit, handleChange, handleInput}) => {
 
-    constructor(props) {
-        super(props)
-        this.state = {
-            query: '',
-            currency:'',
-            results: []
-        }
-
-        this.handleInput = this.handleInput.bind(this);
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-        
-    }
-
-    getInfo = () => {
-        //fetch request?
-    }
-
-    handleInput(event) {
-        this.setState({ text: event.target.value })
-    }
-
-    handleChange(event) {
-        this.setState({ currency: event.target.value })
-    }
-
-    handleSubmit(event) {
-        event.preventDefault();
-        console.log('form submitted');
-        const text = this.state.text.trim();
-        const currency = this.state.currency;
-        if (!text || !currency) {
-            return;
-        }
-
-        console.log('text:',text);
-        console.log('currency:', currency);
-        this.setState({ text: '' , currency: ''})
-    }
-
-    render() {
-        return (
-            <div>
-                <form onSubmit={this.handleSubmit}>
-                    <input
-                        type="text"
-                        placeholder="Search product..."
-                        value={this.state.text}
-                        onChange={this.handleInput}
-                    />
-                    <select id='currency-seelctor' value={this.state.currency} onChange={this.handleChange}>
-                        <option default value="default">Select currency</option>
-                        <option value="GBP">£ - GBP</option>
-                        <option value="USD">$ - USD</option>
-                        <option value="EURO">€ - EURO</option>
-                        <option value="Yen">¥ - JPY</option>
-                        <option value="Yuan">¥ - CNY</option>
-                        <option value="Real">R$ - BRL</option>
-                        <option value="Rupee">₹ - INR</option>
-                        <option value="SinDollar">$ - SGD</option>
-                        <option value="Lira">₺ - TRY</option>
-                        <option value="Peso">Mex$ - MXN</option>
-                        <option value="AUDollar">$ - AUD</option>
-                        
-                    </select>
-                    <input type="submit" value="Search" />
-                    {/* <p>{this.state.query}</p> */}
-                </form>
-            
-                {/* <select id='currency-seelctor' defaultValue="default">
-                    <option disabled value="default">Select currency</option>
-                    <option value="GBP">Pound Sterling</option>
-                    <option value="USD">US Dolar</option>
-                    <option value="EURO">Euro</option>
-                    <option value="Yen">Japanese Yen</option>
-                    <option value="Yuan">Chinese Yuan</option>
-                    <option value="Real">Brazilian Real</option>
-                    <option value="Rupee">Indian Rupee</option>
-                    <option value="SinDollar">Singapore Dollar</option>
-                    <option value="Lira">Turkish Lira</option>
-                    <option value="Peso">Mexican Peso</option>
-                    <option value="AUDollar">Australian Dollar</option>
-                </select> */}
-            </div>
-        )
-    }
+    return (
+        <div>
+            <form onSubmit={handleSubmit}>
+                <input
+                    type="text"
+                    placeholder="Search product..."
+                    value={search}
+                    onChange={handleInput}
+                />
+                <select id='currency-seelctor' value={currency} onChange={handleChange}>
+                    <option default value="default">Select currency</option>
+                    <option value="GBP">£ - GBP</option>
+                    <option value="USD">$ - USD</option>
+                    <option value="EURO">€ - EURO</option>
+                    <option value="Yen">¥ - JPY</option>
+                    <option value="Yuan">¥ - CNY</option>
+                    <option value="Real">R$ - BRL</option>
+                    <option value="Rupee">₹ - INR</option>
+                    <option value="SinDollar">$ - SGD</option>
+                    <option value="Lira">₺ - TRY</option>
+                    <option value="Peso">Mex$ - MXN</option>
+                    <option value="AUDollar">$ - AUD</option>
+                </select>
+                <input type="submit" value="Search" />
+            </form>
+        </div>
+    )
+    
 }
 
 export default SearchBox;
