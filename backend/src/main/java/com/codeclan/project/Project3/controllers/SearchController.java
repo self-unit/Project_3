09207594fef81d2286b0154ba0d00b15a6699d0d
self-unit <self_unit@hotmail.com>
2018-11-05 15:40:46 +0000

    commit progress

--- backend/src/main/java/com/codeclan/project/Project3/controllers/SearchController.java
+++ backend/src/main/java/com/codeclan/project/Project3/controllers/SearchController.java
@@ -1,10 +1,18 @@
 package com.codeclan.project.Project3.controllers;
 
-
+import com.codeclan.project.Project3.models.Search;
+import org.springframework.http.HttpEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(value = "/request/{search}")
 public class SearchController {
+
+    private static final String TEMPLATE = "Hello, %s!";
+
+    @RequestMapping("/search")
+    public HttpEntity<Search> search(
+            @RequestParam(value = "name", required=false, defaultValue = "Word") String name)
+    )
 }
---
+++ backend/src/main/java/com/codeclan/project/Project3/models/Search.java
@@ -0,0 +1,19 @@
+package com.codeclan.project.Project3.models;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.springframework.hateoas.ResourceSupport;
+
+public class Search extends ResourceSupport {
+
+    private final String content;
+
+    @JsonCreator
+    public SearchItem(@JsonProperty("content") String content) {
+        this.content = content;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
