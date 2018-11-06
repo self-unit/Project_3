f4d7bc4cc756b7ab73618f63615b2619cb78ee2a
self-unit <self_unit@hotmail.com>
2018-11-06 13:52:32 +0000

    add json propertie

--- backend/src/main/java/com/codeclan/project/Project3/resources/Country.java
+++ backend/src/main/java/com/codeclan/project/Project3/resources/Country.java
@@ -1,6 +1,7 @@
 package com.codeclan.project.Project3.resources;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import org.springframework.hateoas.ResourceSupport;
 
 public class Country extends ResourceSupport {
@@ -10,7 +11,7 @@ public class Country extends ResourceSupport {
     private String link;
 
     @JsonCreator
-    public Country(String countryCode, double price, String link) {
+    public Country(@JsonProperty("country_code") String countryCode, @JsonProperty("price") double price, @JsonProperty("link") String link) {
         this.countryCode = countryCode;
         this.price = price;
         this.link = link;
