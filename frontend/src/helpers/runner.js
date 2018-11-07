1e4e14901804de7e090816272d14743af6f930ff
Ben <benharvie94@gmail.com>
2018-11-07 12:34:55 +0000

    General cleanup

--- frontend/src/helpers/runner.js
+++ frontend/src/helpers/runner.js
@@ -2,4 +2,3 @@ const CurrencyHelper = require('./CurrencyHelper');
 
 const currency = new CurrencyHelper();
 console.log(currency.convert('.co.uk', 'USD', 55));
-// console.log(currency.getCurrencyFromDomain('.co.uk'));
